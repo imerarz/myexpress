a41f1617c3ddabbac77a8024093bea1c1aa5813a
Eduardo Ramirez <edvramirezr@gmail.com>
2017-06-10 15:15:38 -0400

    Using routing and public folder

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const router = express.Router();
 
-app.get('/', function(req, res) {
+// Static folder for serving js and css files
+router.use(express.static(path.join(__dirname, 'public')));
+
+router.use(function (req, res, next) {
+  console.log('Time:', Date.now())
+  next()
+});
+
+router.get('/', function(req, res) {
   res.send('Hello World!');
 });
+
+app.use('/', router);
 
 app.listen(3000, function(){
   console.log('Express App running on port 3000!');
