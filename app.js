c05b64a87f5db051c439f7bed24eaba43246c5e9
Eduardo Ramirez <edvramirezr@gmail.com>
2017-06-10 15:20:48 -0400

    Using access log with Morgan

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const router = express.Router();
+const morgan = require('morgan');
 
 // Static folder for serving js and css files
 router.use(express.static(path.join(__dirname, 'public')));
 
-router.use(function (req, res, next) {
-  console.log('Time:', Date.now())
-  next()
-});
+router.use(morgan('tiny'));
 
 router.get('/', function(req, res) {
   res.send('Hello World!');
