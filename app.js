6baeff6eb7a18310e5dd0bceda517e986f756f2e
Eduardo Ramirez <edvramirezr@gmail.com>
2017-06-11 15:21:42 -0400

    Created controller for index

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const logger = require('morgan');
 
 const app = express();
 
+const index = require('./controllers/index');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -14,9 +16,12 @@ app.use(logger('dev'));
 // static Files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// controllers
+app.use('/', index);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
